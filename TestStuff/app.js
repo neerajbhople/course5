1a8dd4fe8fd42c9d401e6b00378b98e53c15cc00
Neeraj <neeraj_bhople@rediffmail.com>
2018-10-13 11:28:22 +0530

    Calculate string value

--- TestStuff/app.js
+++ TestStuff/app.js
@@ -1,7 +1,19 @@
 (function(){
-	'use strict'
+	'use strict';
 	angular.module('myFirstApp', [])
-	.controller('myFirstController', function(){
-
+	.controller('myFirstController', function($scope){
+		$scope.name = "";
+		$scope.nameValue = 0;
+		$scope.calculateNameValue = function(){
+			var nameValue = 0;
+  			$scope.nameValue = calculateStringValue($scope.name);
+		};
+		function calculateStringValue(string) {
+  				var totalStringValue = 0;
+  				for (var i = 0; i < string.length; i++) {
+    				totalStringValue += string.charCodeAt(i);
+  				}
+  				return totalStringValue;
+  		};
 	});
-}();
+})();
